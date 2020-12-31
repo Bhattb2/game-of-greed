ef1fe6834238f8c581704e8a60c3fa44cf28de14
Logan Jones <loganemilyjones@mac.com>
2020-12-30 16:04:38 -0800

    calculate score all tests passing

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -4,31 +4,36 @@ class GameLogic:
     
     @staticmethod
     def calculate_score(roll:tuple) -> int:
-        roll = Counter(roll)
-        roll_common = roll.most_common()
+        roll_common = Counter(roll).most_common()
         roll_counts = len(roll_common)
-        most_common_num = roll_common[0][0]
-        most_common_amt = roll_common[0][1]
         base_points = {1: 1000, 2:100, 3:100, 4:100, 5:100, 6:100}
         single_base_points = {1: 100, 5:50}
-
-        print("rolled: ", roll, roll_common, roll_counts)
+        score = 0
 
         #straight
-        if roll_counts is 6:
+        if roll_counts == 6:
             return 1500
 
-        #three, four, five of a kind
-        if most_common_amt >= 3:
-            base = most_common_num * base_points[most_common_num]
-            extra = most_common_amt - 3
+        #three pairs
+        if roll_counts == 3 and roll_common[0][1] == 2 and roll_common[1][1] == 2:
+            return 1500
 
-            return base + (extra * base)
+        #score highest count die, remove, repeat
+        while roll_common:
+            most_common_num = roll_common[0][0]
+            most_common_amt = roll_common[0][1]
+            
+            #three, four, five of a kind
+            if most_common_amt >= 3:
+                base = most_common_num * base_points[most_common_num]
+                extra = most_common_amt - 3
+                score += base + (extra * base)
 
-        # single 1s and 5s
-        if most_common_amt < 3 and most_common_num is 5 or most_common_num is 1:
-            return most_common_amt * single_base_points[most_common_num]
- 
-        return 0
+            # single 1s and 5s
+            elif most_common_amt < 3 and (most_common_num == 5 or most_common_num == 1):
+                score += most_common_amt * single_base_points[most_common_num]
 
-        # next step: Track dice. In each return (except straight and 0) pass unscored die back through recursively.
+            # remove highest die
+            roll_common = roll_common[1:]
+
+        return score
